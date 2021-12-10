f4d33ddfa5f640517db1c562cc5c813a33d1f597
kamran <kamran.davar.kd@gmail.com>
2021-12-10 11:20:37 +0330

    less structure added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './assets/styles/app.less';
 import {Provider} from 'react-redux'
 import configureStore from "./redux/configureStore";
 import Router from './routes'
+import './assets/styles/styles.less';
 
 const store = configureStore();
 
 const App = () => (<>
         <Provider store={store}>
-           <Router/>
+            <Router/>
         </Provider>
     </>
 );
